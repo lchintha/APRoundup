d604d56525c83aaa42e39fd6be1bff277e10fada
lchintha <chintap94@gmail.com>
2021-12-25 21:41:01 -0600

    Project upgraded to target latest version.

--- app/src/main/java/com/vrctech/aproundup/activities/LaunchActivity.java
+++ app/src/main/java/com/vrctech/aproundup/activities/LaunchActivity.java
@@ -231,7 +231,7 @@ public class LaunchActivity extends BaseActivity implements NavigationView.OnNav
         Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
         //intent.setAction("NOTIFICATION");
         intent.putExtra(NotificationReceiver.REQUEST_CODE, requestCode);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), requestCode, intent, PendingIntent.FLAG_IMMUTABLE);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
     }
